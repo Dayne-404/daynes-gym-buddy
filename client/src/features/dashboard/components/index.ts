0f0039e8cb9b771efe70eb7f468abde83cf3cd81
Dayne <daynedell@gmail.com>
2026-05-09 22:56:25 -0400

    Add Barrel files for exports

---
+++ client/src/features/dashboard/components/index.ts
@@ -0,0 +1,8 @@
+export { default as CalorieRing } from "./CalorieRing";
+export { default as DashboardCard } from "./DashboardCard";
+export { default as DashboardHeader } from "./DashboardHeader";
+export { default as ProgressPhoto } from "./ProgressPhoto";
+export { default as RoutineCard } from "./RoutineCard";
+export { default as RoutinesList } from "./RoutinesList";
+export { default as StatsRow } from "./StatsRow";
+export { default as WeightTrend } from "./WeightTrend";
